5f068016760cb1091b1207b8c4f765d7ab617941
daichihattori <p3q24c2o@s.okayama-u.ac.jp>
2021-07-19 13:04:28 +0900

    change macro

---
+++ test/print_macro.c
@@ -0,0 +1,16 @@
+#include<ELiPS/bls12.h>
+int main(){
+    printf("FPLIMB=%d\n",FPLIMB);
+    printf("FPLIMB=%d\n",FPLIMB2);
+    printf("FPLIMB=%d\n",FPLIMB_BITS);
+
+    printf("FRLIMB=%d\n",FRLIMB);
+    printf("FRLIMB=%d\n",FRLIMB2);
+    printf("FRLIMB=%d\n",FRLIMB_BITS);
+
+    printf("FXLIMB=%d\n",FXLIMB);
+    printf("FXLIMB=%d\n",FXLIMB2);
+    printf("FXLIMB=%d\n",FXLIMB_BITS);
+
+
+}
